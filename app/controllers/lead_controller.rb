b3bdb21da483668bad93bf390178d6471f3f07fa
Steve Collard <stevecollard@icloud.com>
2018-10-24 09:04:49 -0400

    lead form work and progress

--- app/controllers/lead_controller.rb
+++ app/controllers/lead_controller.rb
@@ -7,12 +7,13 @@ class LeadController < ApplicationController
         p = params["lead"].permit!
         puts "PARAMS = #{p}"
         file_attachment = p["file_attachment"] 
-        p["file_attachment"] = file_attachment.read
+        #p["file_attachment"] = file_attachment.read
         lead = Lead.new(p)
         lead.valid?
         p lead.errors
-        ZendeskAPI::Ticket.new($client, :id => 1, :priority => "urgent") # doesn't actually send a request, must explicitly call #save! 
-        ZendeskAPI::Ticket.create!($client, :subject => "#{params['lead'][:company_name]}", :comment => { :value => "You have received a new quote submission from #{params['lead'][:full_name]}\nEmail: #{params['lead'][:email]}\nPhone: #{params['lead'][:phone]}\nCompany Name: #{params['lead'][:company_name]}\nProject Name: #{params['lead'][:project_name]}\nProject Description: #{params['lead'][:project_description]}\nDepartment: #{params['lead'][:department]}\nMessage: #{params['lead'][:message]}\n" }, :submitter_id => 123, :priority => "urgent")
+        ZendeskAPI::Ticket.new($client, :id => 1, :priority => "normal") # doesn't actually send a request, must explicitly call #save! 
+        ZendeskAPI::Ticket.create!($client, :subject => "#{params['lead'][:company_name]}", :comment => "#{params['lead'][:message]}", :submitter_id => 123, :priority => "normal", :type => "question", :group => "lead")
+       
         lead.save!
     end
 
