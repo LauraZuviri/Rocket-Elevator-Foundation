a58b210526a85bbbbbfb799e7a5482bebd245db7
jpboutin99 <42749424+jpboutin99@users.noreply.github.com>
2018-10-26 14:14:30 -0400

    addCSV

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_23_114831) do
+ActiveRecord::Schema.define(version: 2018_10_23_154604) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "address_type"
@@ -64,6 +64,8 @@ ActiveRecord::Schema.define(version: 2018_10_23_114831) do
     t.string "technician_phone"
     t.string "technician_email"
     t.string "building_name"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["address_id"], name: "index_buildings_on_address_id"
     t.index ["customer_id"], name: "index_buildings_on_customer_id"
   end
@@ -137,6 +139,7 @@ ActiveRecord::Schema.define(version: 2018_10_23_114831) do
     t.binary "file_attachment", limit: 16777215
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "attachment_file"
     t.index ["customer_id"], name: "index_leads_on_customer_id"
   end
 
@@ -187,7 +190,7 @@ ActiveRecord::Schema.define(version: 2018_10_23_114831) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  create_table "versions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci", force: :cascade do |t|
+  create_table "versions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "item_type", limit: 191, null: false
     t.integer "item_id", null: false
     t.string "event", null: false
